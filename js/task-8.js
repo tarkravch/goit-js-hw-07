5bf69922250e5d52a818b4600dad332d4bf661b6
tarkravch <tarkravch@gmail.com>
2021-02-27 20:38:25 +0200

    final

--- js/task-8.js
+++ js/task-8.js
@@ -1,63 +1,54 @@
-// Я це завдання так і не зробив. може підкажете, що не так.
-// НЕ можу зрозуміти, як тут передати результат функції назовні чи навпаки,аргумент у функцію.
-
-
-const numberOfBoxes = document.querySelector('[type="number"]');
+const numberOfBoxes = document.querySelector('#controls input');
 const boxes = document.querySelector('#boxes');
-
 const render = document.querySelector('[data-action="render"]');
 const destroy = document.querySelector('[data-action="destroy"]');
 
-render.addEventListener('click', createBoxes);
+
 destroy.addEventListener('click', removeBoxes);
-numberOfBoxes.addEventListener('input', onValueInput);
+numberOfBoxes.addEventListener('input', onInputValue);
+render.addEventListener('click', () => createBoxes(boxQuantity));
 
-let boxesQuantity = 0;
 
-//numberOfBoxes.value = amount ???????;
-/* const amount = boxesQuantity;
-console.log(amount); */
 
-function onValueInput(event) {
-    boxesQuantity = Number(event.target.value);
-    console.log(boxesQuantity);
+let boxQuantity = 0;
 
-    if (boxesQuantity >= 1 && boxesQuantity <= 100) {
-        console.log(boxesQuantity);
-        return boxesQuantity;
-    }
+function onInputValue(event) {
+    boxQuantity = event.currentTarget.value;
+    console.log(boxQuantity);
+
+    return boxQuantity;
 };
 
+console.log(boxQuantity);
+
+const amount = boxQuantity;
 
 function createBoxes(amount) {
-
-    const box = document.createElement('div');
-
-    let width = 30;
-    let height = 30;
-    box.style.width = `${width}px`;
-    box.style.height = `${height}px`;
-    let x = Math.floor(Math.random() * 256);
-    let y = Math.floor(Math.random() * 256);
-    let z = Math.floor(Math.random() * 256);
-    box.style.backgroundColor = `rgb(${x}, ${y}, ${z})`;
-
-    if (amount === 1) {
-        boxes.append(box);
-    };
     if (amount >= 1) {
+        let width = 30;
+        let height = 30;
+        const arrayBoxes = [];
         for (let i = 1; i <= amount; i += 1) {
+            const box = document.createElement('div');
+            let x = Math.floor(Math.random() * 256);
+            let y = Math.floor(Math.random() * 256);
+            let z = Math.floor(Math.random() * 256);
+            box.style.width = `${width}px`;
+            box.style.height = `${height}px`;
+            box.style.backgroundColor = `rgb(${x}, ${y}, ${z})`;
+            arrayBoxes.push(box);
             width += 10;
             height += 10;
-            boxes.append(box);
         }
-
+        boxes.append(...arrayBoxes);
     };
-
 };
-
-
 
 function removeBoxes() {
-    boxes.remove();
+    boxes.innerHTML = '';
 };
+
+
+/* function removeBoxes() { 
+    boxes.remove(); //не можна, бо видаляє повністю весь вузол!!! 
+};  */
